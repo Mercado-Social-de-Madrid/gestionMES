637e88736820a011f3d1df2e9143bb6ac6435b90
Joseba S <jjoseba@gmail.com>
2018-09-19 17:51:51 +0200

    #1: Implemented access control for every url

--- mes/settings.py
+++ mes/settings.py
@@ -56,6 +56,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'core.middleware.LoginRequiredMiddleware',
 ]
 
 ROOT_URLCONF = 'mes.urls'
