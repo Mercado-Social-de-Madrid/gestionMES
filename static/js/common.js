db5aa177f69997fd22fae87481b326347d8543ef
Joseba S <jjoseba@gmail.com>
2018-09-25 10:51:48 +0200

    #4: Added AjaxResponseMixin for paginated and ajax views

--- static/js/common.js
+++ static/js/common.js
@@ -1,6 +1,89 @@
 
 var TOAST_DELAY = 1200;
 
+
+function getCookie(name) {
+    var cookieValue = null;
+    if (document.cookie && document.cookie != '') {
+        var cookies = document.cookie.split(';');
+        for (var i = 0; i < cookies.length; i++) {
+            var cookie = jQuery.trim(cookies[i]);
+            // Does this cookie string begin with the name we want?
+            if (cookie.substring(0, name.length + 1) == (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
+}
+
+(function( $ ) {
+    function loadResults(resultsContainer, url){
+        if (url == null || url == '' || url.startsWith('#')){
+            return;
+        }
+        resultsContainer.addClass('loading-container');
+        $.get(url, {}, function(data){
+            resultsContainer.find('.results').html(data);
+            resultsContainer.find('[data-toggle="tooltip"]').tooltip();
+            resultsContainer.find(".link-row").click(function() {
+                window.location = $(this).data("href");
+            });
+            resultsContainer.removeClass('loading-container');
+            var preserveHistory = resultsContainer.attr('data-preservehistory');
+            if (!preserveHistory || preserveHistory != 'true')
+                window.history.replaceState({}, '', url);
+        });
+    }
+
+    $.fn.ajaxLoader = function( url_or_action ) {
+        var self = this;
+
+        if ( url_or_action != null) {
+
+            if (url_or_action === 'reload'){
+                var current = self.find('.pagination .page-item.active > a').attr('href');
+                loadResults(self, current);
+                return self;
+            }
+
+            loadResults(self, url);
+            return self;
+        }
+
+        var initialUrl = self.attr('data-initial');
+        if ((initialUrl != null) && (initialUrl!='')){
+            loadResults(self, initialUrl);
+        }
+
+        self.on('click', '.pagination a', function(e){
+            e.preventDefault();
+            if ($(this).parent().hasClass('active'))
+                return;
+            var url = $(this).attr('href')
+            loadResults(self, url);
+        });
+
+    };
+
+}( jQuery ));
+
+function showToast(message, messageClasses){
+    var toast = $('<div class="toast"></div>').text(message).addClass(messageClasses);
+    toast.appendTo('#main-toasts');
+    setTimeout(function(){ toast.fadeOut(); }, TOAST_DELAY);
+}
+
+$.ajaxSetup({
+    beforeSend: function(xhr, settings) {
+        if (!(/^(GET|HEAD|OPTIONS|TRACE)$/.test(settings.type)) && !this.crossDomain) {
+            var csrftoken = getCookie('csrftoken');
+            xhr.setRequestHeader("X-CSRFToken", csrftoken);
+        }
+    }
+});
+
 $(function(){
     
     var menu = $('#navbar-menu');
@@ -8,6 +91,7 @@ $(function(){
     $('.floating-label .form-control').floatinglabel();
     $('.show-at-load').modal('show');
     $('.datepicker').pickdate();
+    $('.ajax-load').ajaxLoader();
 
     $('[data-toggle="tooltip"]').tooltip();
 
