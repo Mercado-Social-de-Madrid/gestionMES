38b8da13143b0e87656c33e01ae3686715c1e9cd
Joseba S <jjoseba@gmail.com>
2019-01-03 14:59:32 +0100

    Added process workflow models

--- simple_bpm/models.py
+++ simple_bpm/models.py
@@ -4,6 +4,7 @@ from __future__ import unicode_literals
 from django.utils.translation import gettext as _
 from django.db import models
 
+from core.models import User
 from mes import settings
 
 
@@ -36,6 +37,7 @@ class ProcessStep(models.Model):
     class Meta:
         verbose_name = _('Paso de un proceso')
         verbose_name_plural = _('Pasos de un proceso')
+        ordering = ['order']
 
     def __str__(self):
         return "{} - {}".format(self.process.title, self.title).encode('utf-8')
@@ -54,7 +56,9 @@ class ProcessStepTask(models.Model):
 # Class to define the current process established for each of the bussiness processes defined
 class CurrentProcess(models.Model):
 
+    shortname = models.CharField(primary_key=True, unique=True, verbose_name=_('Nombre corto'), max_length=50)
     title = models.CharField(null=True, blank=True, verbose_name=_('Título'), max_length=250)
+    process = models.ForeignKey(Process, null=True, verbose_name=_('Proceso asociado'))
 
     class Meta:
         verbose_name = _('Proceso asignado')
@@ -62,3 +66,76 @@ class CurrentProcess(models.Model):
         permissions = (
             ("mespermission_can_manage_current_process", _("Puede gestionar los procesos asociados a cada tarea")),
         )
+
+# Class to define the concrete step inside a process
+class CurrentProcessStep(models.Model):
+    process = models.ForeignKey(Process, verbose_name=_('Proceso asociado'))
+    shortname = models.CharField(primary_key=True, unique=True, verbose_name=_('Nombre corto'), max_length=50)
+
+    class Meta:
+        verbose_name = _('Paso asignado del proceso')
+        verbose_name_plural = _('Paso asignado del proceso')
+
+
+class ProcessWorkflow(models.Model):
+    process = models.ForeignKey(Process, verbose_name=_('Proceso que sigue'))
+    start_time = models.DateTimeField(auto_now_add=True, verbose_name=_('Fecha de inicio'))
+    current_state = models.ForeignKey(ProcessStep, verbose_name=_('Paso actual'))
+    completed = models.BooleanField(default=False, verbose_name=_('Completado'))
+
+    class Meta:
+        verbose_name = _('Workflow de Proceso')
+        verbose_name_plural = _('Workflows de proceso')
+
+    def add_comment(self, user, comment):
+
+        event = ProcessWorkflowEvent()
+        event.workflow = self
+        event.step = None
+        event.completed_by = user
+        event.comment = comment
+        event.save()
+
+    def complete_current_step(self, user):
+        order = self.current_state.order if self.current_state != None else 0
+        next_step = ProcessStep.objects.filter(process=self.process, order__gt=order, order_by=order).first()
+
+        event = ProcessWorkflowEvent()
+        event.workflow = self
+        event.step = self.current_state
+        event.completed_by = user
+        event.save()
+
+        if next_step is None:
+            # We are in the last step!
+            self.current_state = None
+            self.completed = True
+            self.save()
+        else:
+            self.current_state = next_step
+            self.save()
+
+
+
+class ProcessWorkflowEvent(models.Model):
+    workflow = models.ForeignKey(ProcessWorkflow, verbose_name=_('Evento de un proceso'), related_name='history_events')
+    step = models.ForeignKey(ProcessStep, null=True, verbose_name=_('Paso del proceso'))
+    completed_by = models.ForeignKey(User, verbose_name=_('Usuario'))
+    timestamp = models.DateTimeField(auto_now_add=True, verbose_name=_('Fecha'))
+    comment = models.TextField(blank=True, null=True, verbose_name=_('Comentario'))
+
+    class Meta:
+        verbose_name = _('Evento de Proceso')
+        verbose_name_plural = _('Eventos de proceso')
+
+
+class ProcessWorkflowTask(models.Model):
+    workflow = models.ForeignKey(ProcessWorkflow, verbose_name=_('Evento de un proceso'), related_name='completed_checklist')
+    task = models.ForeignKey(ProcessStepTask, null=True, verbose_name=_('Tarea completada'))
+    completed_by = models.ForeignKey(User, verbose_name=_('Usuario'))
+    timestamp = models.DateTimeField(auto_now_add=True, verbose_name=_('Fecha'))
+
+    class Meta:
+        verbose_name = _('Tarea completadas del proceso')
+        verbose_name_plural = _('Tareas completadas del proceso')
+
