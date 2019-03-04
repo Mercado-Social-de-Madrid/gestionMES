ce9b069d488feb66f9e95525907ae3d0f75d4427
Joseba S <jjoseba@gmail.com>
2019-03-04 17:09:57 +0100

    Better management admin display list for currency app user

--- currency/admin.py
+++ currency/admin.py
@@ -6,6 +6,14 @@ from django.contrib import admin
 # Register your models here.
 from currency.models import GuestInvitation, GuestAccount, CurrencyAppUser
 
+
+class AppUserAdmin(admin.ModelAdmin):
+    list_display = ('account', 'username', 'cif')
+    search_fields = (
+        'cif',
+    )
+
+
 admin.site.register(GuestInvitation)
 admin.site.register(GuestAccount)
-admin.site.register(CurrencyAppUser)
+admin.site.register(CurrencyAppUser, AppUserAdmin)
