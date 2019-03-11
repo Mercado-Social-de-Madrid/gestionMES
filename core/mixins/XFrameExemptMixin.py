7685be05b91d81d8f67caaf39bb852aab7fc8b6e
Joseba S <jjoseba@gmail.com>
2019-03-11 13:05:39 +0100

    Correctly configure X-Frame options

--- core/mixins/XFrameExemptMixin.py
+++ core/mixins/XFrameExemptMixin.py
@@ -1,7 +1,8 @@
+from django.views import View
 from django.views.decorators.clickjacking import xframe_options_exempt
 
 
-class XFrameOptionsExemptMixin:
+class XFrameOptionsExemptMixin(View):
     @xframe_options_exempt
     def dispatch(self, *args, **kwargs):
-        return super().dispatch(*args, **kwargs)
+        return super(XFrameOptionsExemptMixin, self).dispatch(*args, **kwargs)
