904e3c736c5fb1a5d81ab0916b4f47c4c1aedab4
Joseba S <jjoseba@gmail.com>
2019-03-25 10:22:40 +0100

    Avoid using pip to check for installed packages, use `pkg_resources` instead

--- helpers/__init__.py
+++ helpers/__init__.py
@@ -1,14 +1,10 @@
 __all__ = ['decorators', 'fcm', 'filesystem', 'query', 'mailing']
 
-import pip
+import pkg_resources
 
 def is_package_installed(package_name):
-    if hasattr(pip, 'get_installed_distributions'):
-        loader = pip.get_installed_distributions
-    else:
-        loader = pip._internal.utils.misc.get_installed_distributions
 
-    installed_packages = loader()
+    installed_packages = pkg_resources.working_set
     packages = [package.project_name for package in installed_packages]
     return package_name in packages
 
