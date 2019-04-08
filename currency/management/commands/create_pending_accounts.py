1d076ec412f74e592f596ad43a3f6419dcdf6069
Joseba S <jjoseba@gmail.com>
2019-04-08 13:51:44 +0200

    Added management command to create currency app users to all pending consumers

---
+++ currency/management/commands/create_pending_accounts.py
@@ -0,0 +1,33 @@
+import json
+import re
+
+import requests
+from django.contrib.auth.models import User
+from django.core.management.base import BaseCommand
+from django.db import IntegrityError
+
+
+from accounts.models import Provider, TERR_LOCAL, TERR_COUNTRY, LegalForm, Consumer, ACTIVE
+from currency_server import create_account
+from mes import settings
+
+
+class Command(BaseCommand):
+    help = 'Create account in currency server for every user that is missing one'
+
+    def handle(self, *args, **options):
+
+        consumers = Consumer.objects.filter(status=ACTIVE, app_user__isnull=True)
+        failed = []
+        for consumer in consumers:
+            try:
+                create_account.post_consumer(consumer)
+            except:
+                print '{}: Failed!'.format(consumer)
+                failed.append(consumer)
+
+
+        print 'Process completed!'
+        print '{} fails'.format(len(failed))
+        for consumer in failed:
+            print consumer
