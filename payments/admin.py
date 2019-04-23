1f542a73a6fa79d82b3d06fc267eda1b2b2e04bd
Joseba S <jjoseba@gmail.com>
2019-04-23 12:03:04 +0200

    Only show sandbox alert when really in sandbox payment

--- payments/admin.py
+++ payments/admin.py
@@ -5,7 +5,8 @@ from django.contrib import admin
 
 # Register your models here.
 from accounts.models import LegalForm, Category, SignupProcess, Provider
-from payments.models import FeeRange, PendingPayment
+from payments.models import FeeRange, PendingPayment, CardPayment
 
 admin.site.register(FeeRange)
 admin.site.register(PendingPayment)
+admin.site.register(CardPayment)
