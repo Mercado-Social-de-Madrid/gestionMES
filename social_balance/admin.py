d8fafe6c8972eb18f73fe14270772e5efdc60f94
Joseba S <jjoseba@gmail.com>
2019-10-16 13:08:24 +0200

    View social badge preview

--- social_balance/admin.py
+++ social_balance/admin.py
@@ -4,10 +4,11 @@ from __future__ import unicode_literals
 from django.contrib import admin
 
 # Register your models here.
-from social_balance.models import EntitySocialBalance
+from social_balance.models import EntitySocialBalance, SocialBalanceBadge
 
 
 class BalanceAdmin(admin.ModelAdmin):
     list_display = ('entity', 'year', 'is_exempt', 'done')
 
 admin.site.register(EntitySocialBalance, BalanceAdmin)
+admin.site.register(SocialBalanceBadge)
