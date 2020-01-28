52b88732151be46e4d76fe944258464e004b01ae
Joseba S <jjoseba@gmail.com>
2020-01-28 13:15:51 +0100

    Fix issue with intercoop form and privacy check

--- intercoop/forms/account.py
+++ intercoop/forms/account.py
@@ -9,10 +9,18 @@ class IntercoopAccountForm(forms.ModelForm, BootstrapForm):
     class Meta:
         model = IntercoopAccount
         exclude = []
-
+        widgets = {
+            'address': forms.Textarea(attrs={'rows': 3}),
+        }
 
 class IntercoopAccountSignupForm(BaseSignupForm):
+
+    check_share_data = forms.BooleanField(required=True,
+                                              widget=forms.CheckboxInput(attrs={'class': 'custom-control-input'}))
 
     class Meta:
         model = IntercoopAccount
         exclude = []
+        widgets = {
+            'address': forms.Textarea(attrs={'rows': 3}),
+        }
