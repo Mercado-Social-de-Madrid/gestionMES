c0f15c33d31dfc1df22cf95f3b75fa9c76901042
Joseba S <jjoseba@gmail.com>
2020-05-24 19:17:52 +0200

    Update timezone issues in cron

--- currency_server/fetch_account_info.py
+++ currency_server/fetch_account_info.py
@@ -77,7 +77,7 @@ def fetch_account(account):
             if 'email' in account_data and account_data['email']:
                 account.contact_email = account_data['email']
             uuid = account_data['id']
-            account.last_updated =  timezone.localtime(timezone.now())
+            account.last_updated =  timezone.now()
             account.save()
 
         elif result['type'] == 'entity':
@@ -119,7 +119,7 @@ def fetch_account(account):
             update_categories(account, account_data)
 
             uuid = account_data['id']
-            account.last_updated =  timezone.localtime(timezone.now())
+            account.last_updated =  timezone.now()
             account.save()
 
         app_user.uuid = uuid
@@ -214,7 +214,7 @@ def fetch_intercoop_account(account):
         if 'email' in account_data and account_data['email']:
             account.contact_email = account_data['email']
 
-        account.last_updated = timezone.localtime(timezone.now())
+        account.last_updated = timezone.now()
         account.save()
         app_user.uuid = account_data['id']
         app_user.save()
