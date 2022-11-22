30ebd35bbbe09618f076aa36e647cb9487fd9798
julio <jberzal86@gmail.com>
2022-11-22 13:10:59 +0100

    MES-24 - Command to set all social capitals as paid

---
+++ accounts/management/commands/set_all_social_capital_paid.py
@@ -0,0 +1,21 @@
+
+from django.core.management.base import BaseCommand
+
+from accounts.models import SocialCapital
+from payments.models import FeeRange
+
+
+class Command(BaseCommand):
+    help = 'All auto-filled social capitals must be marked as paid (see fill_social_capital.py)'
+
+    def handle(self, *args, **options):
+        social_capitals = SocialCapital.objects.all()
+
+        total = len(social_capitals)
+        actual = 0
+
+        for social_capital in social_capitals:
+            actual += 1
+            print(f'Processing {actual} of {total}')
+            social_capital.paid = True
+            social_capital.save()
