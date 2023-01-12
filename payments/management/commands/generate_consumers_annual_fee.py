cb1a9404c9ff31c6ff1aee48108ed7ba02385975
julio <jberzal86@gmail.com>
2023-01-12 13:17:55 +0100

    - added command to generate consumers annual fee charge

---
+++ payments/management/commands/generate_consumers_annual_fee.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+
+from django.core.management.base import BaseCommand
+
+from accounts.models import Consumer
+from payments.models import AccountAnnualFeeCharge, AnnualFeeCharges, PendingPayment
+
+
+class Command(BaseCommand):
+    help = 'Generate annual fee charges for Consumers'
+
+    def add_arguments(self, parser):
+        parser.add_argument('year', type=int, help='Year for annual fee charges generation')
+
+    def handle(self, *args, **options):
+
+        year = options['year']
+        consumers = Consumer.objects.active().filter(registration_date__year__lt=year)
+        annual_charge, created = AnnualFeeCharges.objects.get_or_create(year=year)
+
+        total = len(consumers)
+
+        for index, consumer in enumerate(consumers):
+            charge, created = AccountAnnualFeeCharge.objects.get_or_create(account=consumer, annual_charge=annual_charge, collab=None)
+            fee = consumer.current_fee
+            if fee is not None:
+                if not charge.split and not charge.payment:
+                    print(f'Consumer {index+1} of {total}', end="\r")
+                    concept = consumer.fee_concept(year)
+                    charge.payment = PendingPayment.objects.create(concept=concept, account=consumer, amount=fee)
+                    charge.amount = fee
+                    charge.save()
+                else:
+                    print(f'Consumer fee already created: {consumer.display_name}')
+            else:
+                print(f'Fee is None for {consumer.display_name}')
+
+        print("")
+
