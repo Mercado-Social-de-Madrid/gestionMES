9ed3cb33891873e71576843c5427a50e313ca840
julio <jberzal86@gmail.com>
2023-04-28 11:44:56 +0200

    mes-136 - added regenerate member ids command

---
+++ accounts/management/commands/regenerate_member_ids.py
@@ -0,0 +1,46 @@
+
+from django.core.management.base import BaseCommand
+from accounts.models import Provider, Consumer, SocialCapital, Account
+from intercoop.models import IntercoopAccount
+from payments.models import FeeRange
+
+
+class Command(BaseCommand):
+    help = 'Generate initial member ids'
+
+    def handle(self, *args, **options):
+
+        print("Socias comunes")
+        print("==========================")
+
+        accounts = Account.objects.all()
+
+        print("Eliminando números de socia anteriores")
+        for account in accounts:
+            account.member_id = None
+            account.save()
+
+        for account in accounts:
+            new_member_id = Account.get_new_member_id()
+            account.member_id = new_member_id
+            account.save()
+
+            print('"member_id":"{}", "cif":"{}"'.format(new_member_id, account.cif))
+
+
+        print("\nSocias de intercooperación")
+        print("==========================")
+
+        intercoop = IntercoopAccount.objects.all()
+
+        print("Eliminando números de socia anteriores")
+        for account in intercoop:
+            account.member_id = None
+            account.save()
+
+        for account in intercoop:
+            new_member_id = IntercoopAccount.get_new_member_id()
+            account.member_id = new_member_id
+            account.save()
+
+            print('"member_id":"{}", "cif":"{}"'.format(new_member_id, account.cif))
