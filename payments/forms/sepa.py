4ac94d54170ff162891d0d9964f48f5293979391
Alberto Moreta <albertomoreta70@gmail.com>
2023-06-06 18:01:11 +0200

    MES-148: Replace UpdateBatchForm with update_or_create function

--- payments/forms/sepa.py
+++ payments/forms/sepa.py
@@ -14,23 +14,10 @@ class SepaBatchForm(forms.ModelForm, BootstrapForm):
         model = SepaPaymentsBatch
         exclude = ['sepa_file', 'amount', 'attempt']
 
-
     def save(self, commit=True):
-
         instance = forms.ModelForm.save(self, False)
-        # Prepare a 'save_m2m' method for the form,
-        def save_m2m():
-           paymentsOrder = self.cleaned_data['payments_order']
-           paymentsOrder = paymentsOrder.split(settings.INLINE_INPUT_SEPARATOR)
-           print(paymentsOrder)
-           for payment in self.cleaned_data['payments'].all():
-               order = paymentsOrder.index(str(payment.pk))
-               print(payment)
-               print(payment.pk)
-               print(order)
-               SepaBatchResult.objects.create(payment=payment, order=order, batch=instance)
 
-        self.save_m2m = save_m2m
+        self.save_m2m = self.save_many2many
 
         # Do we need to save all changes now?
         if commit:
@@ -48,22 +35,20 @@ class SepaBatchForm(forms.ModelForm, BootstrapForm):
 
         return instance
 
-
-class UpdateBatchForm(forms.ModelForm, BootstrapForm):
-    payments = forms.ModelMultipleChoiceField(queryset=PendingPayment.objects.filter(), required=False, )
-    payments_order = forms.CharField(widget=forms.HiddenInput())
-
-    class Meta:
-        model = SepaPaymentsBatch
-        exclude = ['sepa_file', 'amount', 'attempt', 'generated_by']
-
-    def save(self, commit=True):
-        instance = forms.ModelForm.save(self, False)
-        if commit:
-            total_amount = 0
-            for payment in self.cleaned_data['payments']:
-                total_amount += payment.amount
-            instance.amount = total_amount
-            instance.save()
-
-        return super().save(self)
+    def save_many2many(self):
+        instance = self.instance
+        non_updated_batch_payments = set(instance.batch_payments.all())
+        paymentsOrder = self.cleaned_data['payments_order']
+        paymentsOrder = paymentsOrder.split(settings.INLINE_INPUT_SEPARATOR)
+        for payment in self.cleaned_data['payments'].all():
+            order = paymentsOrder.index(str(payment.pk))
+            sepa_batch_result, created = SepaBatchResult.objects.update_or_create(
+                payment=payment,
+                batch=instance,
+                defaults={
+                    'order': order
+                }
+            )
+            non_updated_batch_payments -= {sepa_batch_result}  # Ignore batch_payment if updated/created
+        for batch_payment in non_updated_batch_payments:
+            batch_payment.delete()
