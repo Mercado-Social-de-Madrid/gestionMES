5798f0321a15518a37b4cbd5ad57c1b248d2bd3b
Alberto Moreta <albertomoreta70@gmail.com>
2023-06-07 14:28:43 +0200

    MES-139: Only list broken links

--- social_balance/management/commands/list_broken_social_balance_links.py
+++ social_balance/management/commands/list_broken_social_balance_links.py
@@ -1,5 +1,3 @@
-from django.conf import settings
-from django.core.files import File
 from django.core.management import BaseCommand
 from django.http import HttpResponse
 
@@ -7,7 +5,7 @@ from social_balance.models import EntitySocialBalance
 
 
 class Command(BaseCommand):
-    help = 'Fix broken social balance links'
+    help = 'List broken social balance links'
 
     def handle(self, *args, **options):
         social_balances = EntitySocialBalance.objects.filter(report__isnull=False)
@@ -22,10 +20,6 @@ class Command(BaseCommand):
                     url_split = balance.report.url.split('/')
                     if url_split[2] != 'reports':
                         print(f'- {balance.entity.name}, {balance.year}, {balance.report.name}')
-                        corrected_url = '/reports/' + balance.report.name
-                        with open(settings.MEDIA_ROOT + corrected_url, 'rb') as f:
-                            balance.report = File(f)
-                            balance.save()
 
 
 
