084f6829528afd97afc2adc23fcbf84c3ddf82b6
Alberto Moreta <albertomoreta70@gmail.com>
2023-06-09 14:14:42 +0200

    MES-139: Send email to admins if there are broken social balance links

--- social_balance/management/commands/list_broken_social_balance_links.py
+++ social_balance/management/commands/list_broken_social_balance_links.py
@@ -1,7 +1,12 @@
+import logging
+
+from django.core.mail import mail_admins
 from django.core.management import BaseCommand
 from django.http import HttpResponse
 
 from social_balance.models import EntitySocialBalance
+
+logger = logging.getLogger(__name__)
 
 
 class Command(BaseCommand):
@@ -9,7 +14,8 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         social_balances = EntitySocialBalance.objects.filter(report__isnull=False)
-        print('Affected social balance reports:')
+        affected_reports = ''
+        logger.info('Affected social balance reports:')
         for balance in social_balances:
             if balance.report.name:
                 filename = balance.report.name.split('/')[-1]
@@ -19,9 +25,21 @@ class Command(BaseCommand):
                 except FileNotFoundError:
                     url_split = balance.report.url.split('/')
                     if url_split[2] != 'reports':
-                        print(f'- {balance.entity.name}, {balance.year}, {balance.report.name}')
+                        entity_info = f'- {balance.entity.name}, {balance.year}, {balance.report.name}\n'
+                        affected_reports += entity_info
+                        logger.info(entity_info)
+        print(affected_reports)
+        if affected_reports:
+            self.send_email(affected_reports)
 
-
+    def send_email(self, affected_reports):
+        subject = 'ERROR - Hay enlaces rotos en las infografías de las siguientes entidades:'
+        mail_admins(
+            subject=subject,
+            message=affected_reports,
+            fail_silently=True,
+        )
+        logger.info('Email sent to admins.')
 
 
 
