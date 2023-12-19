44485c22463a25e28a9ad372f12303a9b47e80da
Alberto Moreta <albertomoreta70@gmail.com>
2023-12-19 14:06:10 +0100

    Get full path for rewriting file

--- social_balance/models/balance.py
+++ social_balance/models/balance.py
@@ -24,8 +24,9 @@ from django.core.files.storage import FileSystemStorage
 class OverwriteStorage(FileSystemStorage):
 
     def get_available_name(self, name, max_length=None):
-        if self.exists(name):
-            os.remove(os.path.join(settings.MEDIA_ROOT, name))
+        file_path = os.path.join(settings.MEDIA_ROOT, name)
+        if self.exists(file_path):
+            os.remove(file_path)
         return name
 
 
