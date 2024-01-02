08566b348f0a94d5049b477fdde31d9deb77362b
julio <jberzal86@gmail.com>
2024-01-02 20:18:24 +0100

    mes-164 - fixed command for providers (SepaBatchResult)

--- payments/management/commands/generate_providers_annual_fee.py
+++ payments/management/commands/generate_providers_annual_fee.py
@@ -2,8 +2,8 @@
 
 from django.core.management.base import BaseCommand
 
-from accounts.models import Consumer, Provider
-from payments.models import AccountAnnualFeeCharge, AnnualFeeCharges, PendingPayment, SepaPaymentsBatch
+from accounts.models import Provider
+from payments.models import AccountAnnualFeeCharge, AnnualFeeCharges, PendingPayment, SepaPaymentsBatch, SepaBatchResult
 
 
 class Command(BaseCommand):
@@ -22,10 +22,12 @@ class Command(BaseCommand):
 
         sepa = SepaPaymentsBatch.objects.create(title="Cuotas anuales proveedoras " + str(year))
 
+        order = 1
+
         for index, provider in enumerate(providers):
             charge, created = AccountAnnualFeeCharge.objects.get_or_create(account=provider, annual_charge=annual_charge, collab=None)
             fee = provider.current_fee
-            if fee is not None:
+            if fee is not None and fee > 0:
                 if not charge.split and not charge.payment:
                     print(f'Provider {index+1} of {total}', end="\r")
                     concept = provider.fee_concept(year)
@@ -33,14 +35,23 @@ class Command(BaseCommand):
                     charge.amount = fee
                     charge.save()
                     sepa.payments.add(charge.payment)
+                    SepaBatchResult.objects.update_or_create(
+                        payment=charge.payment,
+                        batch=sepa,
+                        defaults={'order': order}
+                    )
+                    order += 1
                 else:
                     print(f'Consumer fee already created: {provider.display_name}')
             else:
-                print(f'Fee is None for {provider.display_name}')
+                print(f'Fee is None or 0 for {provider.display_name}')
 
         if sepa.payments.exists():
+            print("Preparing SEPA...")
             sepa.amount = sum(payment.amount for payment in sepa.payments.all())
             sepa.generated_by = options.get("user")
+            sepa.preprocess_batch()
+            sepa.generate_batch()
             sepa.save()
         else:
             sepa.delete()
