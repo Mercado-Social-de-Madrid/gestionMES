ac06e3bc798c0ffd408829b87dad90ea0a5b1d2e
julio <jberzal86@gmail.com>
2024-04-12 01:56:00 +0200

    mes-179 - Fixed command to check report filenames with HA.

--- social_balance/management/commands/check_report_filename_ha.py
+++ social_balance/management/commands/check_report_filename_ha.py
@@ -1,4 +1,5 @@
 import requests
+import os
 from django.core.management.base import BaseCommand
 
 from accounts.models import Entity
@@ -9,7 +10,7 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
 
-        r = requests.get('https://app.mercadosocial.net/api/v1/entities/')
+        r = requests.get('https://app.mercadosocial.net/api/v1/entities/?limit=2000')
         if r.ok:
             result = r.json()['entities']
 
@@ -17,9 +18,12 @@ class Command(BaseCommand):
             for entity in entities:
                 app_entity = next(filter(lambda x: x["member_id"] == entity.member_id, result), None)
                 if app_entity:
-                    if not entity.report_filename == app_entity['balance_detail']:
-                        print(f'Nombres de archivo de balance no coincide: {entity.name} - {entity.cif} - {entity.member_id}')
-
+                    if app_entity['balance_detail']:
+                        filename = os.path.basename(app_entity['balance_detail'])
+                        if not entity.report_filename == filename:
+                            print(f'Nombres de archivo de balance no coincide: {entity.name} - {entity.cif} - {entity.member_id}\n{entity.report_filename} - {filename}')
+                    # else:
+                    #     print(f'No existe nombre de archivo en HA: {entity.name} - {entity.cif} - {entity.member_id}')
 
         else:
             print('Fallo en la petición de entidades a la HA')
